bd7339056056c13ecffb01aa84267a30b7b053d7
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2022-01-13 13:49:03 +0300

    add router and delete App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./Components/Home";
 
-import styles from "./App.module.scss";
+function Router() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="home" element={<Home />} />
+        </Routes>
+    );
+}
 
 function App() {
     return (
         <div>
+            <Router />
             <Home />
         </div>
     );
